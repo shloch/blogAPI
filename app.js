d305498d45955a83154f2b92f48b617261f12027
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2021-03-15 10:54:47 +0100

    adding comment to code

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-
+/**
+ * Function thats reads the JSON file content (as a text file)
+ * this function returns the file's content as TEXT (string)
+ * It returns NULL if file doesn't exist
+ */
 function readTextFile(file) {
   let answer = null
   let rawFile = new XMLHttpRequest();
@@ -18,19 +22,18 @@ function readTextFile(file) {
 let fileContent = readTextFile('blog.json')
 if (fileContent != null) {
   let blogs = JSON.parse(fileContent)
-  console.log(blogs)
-
 
   //loop through the JSON structure
   let data = ``
 
   for (let blog of blogs) {
     data += `<div class="blogs"> <div class="thumbnail"><img src="thumbnail.png"/></div>
-            <div class="blog">
-              <h3> "${blog.titre}" </h3> 
-              (<span class="author"> Par <strong> ${blog.auteur} </strong>, le ${blog.date}</span> )
-              <p> ${blog.description} </p>
-            </div></div>`
+              <div class="blog">
+                <h3> "${blog.titre}" </h3> 
+                (<span class="author"> Par <strong> ${blog.auteur} </strong>, le ${blog.date}</span> )
+                <p> ${blog.description} </p>
+              </div>
+             </div>`
   }
 
   document.getElementById('main').innerHTML = data;
